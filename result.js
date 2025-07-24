47a24c322b0fa3b5d1f8ac13f4c630036e014b06
JungEunhyo <alyssa.jung0123@gmail.com>
2025-07-24 15:39:58 +0900

    Update result.js

--- result.js
+++ result.js
@@ -10,42 +10,42 @@ function getQueryParams() {
 function getResult({ jureong, soba, parfait }) {
   if (jureong === 5) {
     return {
-      img: 'image/geto_bad.png',
+      img: 'geto_bad.png',
       text: '그는 교주가 되었습니다.',
     };
   } else if (soba === 5) {
     return {
-      img: 'image/geto_teacher.png',
+      img: 'geto_teacher.png',
       text: '그는 주술고전의 선생님이 되었습니다. 해피엔딩!',
     };
   } else if (parfait === 5) {
     return {
-      img: 'image/gojo.png',
+      img: 'gojo.png',
       text: '축하합니다. 고죠 사토루로 진화했습니다!',
     };
   } else if (jureong === 4) {
     return {
-      img: 'image/geto4.png',
+      img: 'geto4.png',
       text: '이런, 정병구루가 되어버렸습니다. 빨리 정신과로!!!',
     };
   } else if (jureong === 3) {
     return {
-      img: 'image/geto3.png',
+      img: 'geto3.png',
       text: '어라, 토우지와 싸우다 죽어버렸습니다. 안돼~~',
     };
   } else if (jureong === 2) {
     return {
-      img: 'image/geto2.png',
+      img: 'geto2.png',
       text: '그는 주술사를 때려치고 하라혼을 결성했습니다!',
     };
   } else if (jureong === 1) {
     return {
-      img: 'image/geto1.png',
+      img: 'geto1.png',
       text: '그는 룩업이 되었습니다. 대체 왜?!',
     };
   } else {
     return {
-      img: 'image/geto0.png',
+      img: 'geto0.png',
       text: `그는 영원히 청춘 속에서 살아간다.`,
     };
   }
